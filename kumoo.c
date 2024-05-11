41e657af43ee1d3dc862857f6099f2f772872753
Hoeun Lee <hoeunlee228@gmail.com>
2024-05-12 05:31:43 +0900

    [Refactor] Add Log and Refactoring kumoo.c

--- kumoo.c
+++ kumoo.c
@@ -7,42 +7,47 @@
 #define EXIT	2
 #define TSLICE	5
 
-struct handlers{
+struct handlers {
        int (*sched)(unsigned short);
        int (*pgfault)(unsigned short);
        int (*exit)(unsigned short);
-}kuos;
+} kuos;
 
-void ku_dump_pmem(void){
-    for(int i = 0; i < (64 << 12); i++){
+void ku_dump_pmem(void) {
+    for (int i = 0; i < (64 << 12); i++) {
         printf("%x ", pmem[i]);
     }
     printf("\n");
 }
-void ku_dump_swap(void){
-    for(int i = 0; i < (64 << 14); i++){
+
+void ku_dump_swap(void) {
+    for (int i = 0; i < (64 << 14); i++) {
         printf("%x ", swaps[i]);
     }
     printf("\n");
 }
 
-void ku_reg_handler(int flag, int (*func)(unsigned short)){
-	switch(flag){
-		case SCHED:
-			kuos.sched = func;
-			break;
-		case PGFAULT:
-			kuos.pgfault = func;
-			break;
-		case EXIT:
-			kuos.exit = func;
-			break;
-		default:
-			exit(0);
+void ku_reg_handler(int flag, int (*func)(unsigned short)) {
+	switch (flag) {
+		case SCHED: {
+            kuos.sched = func;
+            break;
+        }
+		case PGFAULT: {
+            kuos.pgfault = func;
+            break;
+        }
+		case EXIT: {
+            kuos.exit = func;
+            break;
+        }
+		default: {
+            exit(0);
+        }
 	}
 }
 
-int ku_traverse(unsigned short va, int write){
+int ku_traverse(unsigned short va, int write) {
 	int pd_index, pt_index, pa;
 	unsigned short *ptbr;
 	unsigned short *pte, *pde;
@@ -51,7 +56,7 @@ int ku_traverse(unsigned short va, int write){
 	pd_index = (va & 0xFFC0) >> 11;
 	pde = pdbr + pd_index;
 
-	if(!(*pde & 0x1)){
+	if (!(*pde & 0x1)) {
 		return -1;
     }
     
@@ -61,7 +66,7 @@ int ku_traverse(unsigned short va, int write){
 	pt_index = (va & 0x07C0) >> 6;
 	pte = ptbr + pt_index;
 
-	if(!(*pte & 0x1)){
+	if (!(*pte & 0x1)) {
         return -1;
     }
 
@@ -69,48 +74,60 @@ int ku_traverse(unsigned short va, int write){
 
 	pa = (PFN << 6)+(va & 0x3F);
 
-    if (write)
+    if (write) {
         *pte |= 0x2;
+    }
 
 	return pa;
 }
 
 
-void ku_os_init(void){
+void ku_os_init(void) {
     /* Initialize physical memory*/
     pfnum = 1 << 12;
     sfnum = 1 << 14;
+
     pmem = (char*)malloc(64 << 12);
     swaps = (char*)malloc(64 << 14);
+
     /* Init free list*/
     ku_freelist_init();
+
     /*Register handler*/
 	ku_reg_handler(SCHED, ku_scheduler);
 	ku_reg_handler(PGFAULT, ku_pgfault_handler);
 	ku_reg_handler(EXIT, ku_proc_exit);
 }
 
-int op_read(unsigned short pid){
+int op_read(unsigned short pid) {
     unsigned short va;
     int addr, pa, ret = 0;
     char sorf = 'S';
+
     /* get Address from the line */
-    if(fscanf(current->fd, "%d", &addr) == EOF){
+    if (fscanf(current->fd, "%d", &addr) == EOF) {
         /* Invaild file format */
+        printf("[CPU] ERROR (102): Invalid File Format.\n");
         return 1;
     }
+
+    printf("[CPU] <<< READ <- (%d) >>>\n", addr);
+
     va = addr & 0xFFFF;
     pa = ku_traverse(va, 0);
 
-    if (pa < 0){
+    if (pa < 0) {
         /* page fault!*/
         ret = kuos.pgfault(va);
-    	if (ret > 0){
+
+    	if (ret > 0) {
             /* No free page frames or SEGFAULT */
             sorf = 'E';
             ret = kuos.exit(current->pid);
-            if (ret > 0){
+
+            if (ret > 0) {
                 /* invalid PID */
+                printf("[CPU] ERROR (120): Invalid PID.\n");
                 return 1;
             }
         }
@@ -120,7 +137,7 @@ int op_read(unsigned short pid){
         }
     } 
 
-    if (pa < 0){
+    if (pa < 0) {
         printf("%d: %d -> (%c)\n", pid, va, sorf);
         return -1;
     }
@@ -131,27 +148,35 @@ int op_read(unsigned short pid){
   
 }
 
-int op_write(unsigned short pid){
+int op_write(unsigned short pid) {
     unsigned short va;
     int addr, pa, ret = 0;
     char input ,sorf = 'S';
+
     /* get Address from the line */
-    if(fscanf(current->fd, "%d %c", &addr, &input) == EOF){
+    if (fscanf(current->fd, "%d %c", &addr, &input) == EOF) {
         /* Invaild file format */
+        printf("[CPU] ERROR (149): Invalid File Format.\n");
         return 1;
     }
+
+    printf("[CPU] <<< WRITE \"%c\" -> (%d) >>>\n", input, addr);
+
     va = addr & 0xFFFF;
     pa = ku_traverse(va, 1);
 
-    if (pa < 0){
-        /* page fault!*/
+    if (pa < 0) {
+        /* page fault! */
         ret = kuos.pgfault(va);
-    	if (ret > 0){
+
+    	if (ret > 0) {
             /* No free page frames or SEGFAULT */
             sorf = 'E';
             ret = kuos.exit(current->pid);
-            if (ret > 0){
+
+            if (ret > 0) {
                 /* invalid PID */
+                printf("[CPU] ERROR (167): Invalid PID.\n");
                 return 1;
             }
         }
@@ -161,7 +186,7 @@ int op_write(unsigned short pid){
         }
     } 
 
-    if (pa < 0){
+    if (pa < 0) {
         printf("%d: %d -> (%c)\n", pid, va, sorf);
         return -1;
     }
@@ -173,34 +198,38 @@ int op_write(unsigned short pid){
 
 }
 
-int do_ops(char op){
+int do_ops(char op) {
     char sorf;
     int ret;
     unsigned short pid = current->pid;
 
-    switch(op){
-        case 'r':
+    switch (op) {
+        case 'r': {
             ret = op_read(pid);
-        break;
+            break;
+        }
 
-        case 'w':
+        case 'w': {
             ret = op_write(pid);
-        break;
+            break;
+        }
 
-        case 'e':
+        case 'e': {
             ret = kuos.exit(pid);
-            if (ret > 0){
+            if (ret > 0) {
                 /* invalid PID */
+                printf("[CPU] ERROR (209): Invalid PID.\n");
                 return ret;
             }
             return -1;
-        break;
+            break;
+        }
     }
     return ret;
 
 }
 
-void ku_run_procs(void){
+void ku_run_procs(void) {
     char sorf;
 	int addr, pa, i;
     char op;
@@ -208,18 +237,24 @@ void ku_run_procs(void){
     unsigned short pid = 0;
 
     ret = kuos.sched(10);
-    /* No processes */
-    if (ret > 0)
-        return;
-    
-	do{
-		if(!current)
-			exit(0);
 
-		for( i=0 ; i<TSLICE ; i++){
-			    /* Get operation from the line */
-			if(fscanf(current->fd, " %c", &op) == EOF){
+    /* No processes */
+    if (ret > 0) {
+        printf("[CPU] (231): No Ready Process Found.\n");
+        return;
+    }
+    
+	do {
+		if (!current) {
+            printf("[CPU] ERROR (237): Program Counter (current) is NULL.\n");
+            exit(0);
+        }
+
+		for (i = 0; i < TSLICE; i++) {
+            /* Get operation from the line */
+			if (fscanf(current->fd, " %c", &op) == EOF) {
 				/* Invaild file format */
+                printf("[CPU] ERROR (245): Invalid File Format.\n");
 				return;
 			}
 
@@ -227,27 +262,33 @@ void ku_run_procs(void){
 			ret = do_ops(op);
 		
             /* process terminated */
-            if (ret < 0){
+            if (ret < 0) {
                 break;
             }
-            else if( ret > 0){
+            else if (ret > 0) {
                 /* something went wrong */
+                printf("[CPU] ERROR (258): Unknown Error Occurred.\n");
             }
 		}
 
 		ret = kuos.sched(pid);
-        /* No processes */
-        if (ret > 0)
-            return;
 
-	}while(1);
+        /* No processes */
+        if (ret > 0) {
+            printf("[CPU] (265): No Ready Process Found.\n");
+            return;
+        }
+
+	} while (1);
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 	/* System initialization */
 	ku_os_init();
+
 	/* Per-process initialization */
 	ku_proc_init(argc, argv);
+
 	/* Process execution */
 	ku_run_procs();
 
